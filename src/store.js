8b6d172f2055f65f9e71c8b136f34a410398e630
Deysi <sude.90@gmail.com>
2017-10-23 14:48:26 -0500

    store.js

--- src/store.js
+++ src/store.js
@@ -5,30 +5,30 @@ let QUIZ = [
     {
         questions: "Which is the oldest airline in the world?",
         options: [["Avianca", "KLM", "Qantas"]],
-        image: ""
+        image: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/plane.svg"
     },
     {
         questions: "Which is the largest port in the world?",
         options: ["Port of Shanghai", "Port of Singapore", "Port of Rotterdam"],
-        image: ""
+        image: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/ship.svg"
 
     },
     {
         questions: "What is the longest distance cycling backwards?",
         options: ["89.30 km", "675.10 km", "337.60 km"],
-        image: ""
+        image: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/bycicle.svg"
 
     },
     {
         questions: "What is the highest speed ever reached by a school bus?",
         options: ["590 km/h", "320 km/h", "245 km/h"],
-        image: ""
+        image: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/bus.svg"
 
     },
     {
         questions: "What is the longest car trip on one tank of gas?",
         options: ["2617 km", "3568 km", "1732 km"],
-        image: ""
+        image: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/car.svg"
     }
 ];
 const answers = [];
@@ -42,15 +42,4 @@ const InitialState = {
 
 const store = createStore(initialState);
 export default store;
-/* {
-    this.images = ["plane.svg","ship.svg","bicycle.svg","bus.svg","car.svg","truck.svg"];
-    this.index = 0;
-    this.questions = ["What is the highest speed ever reached by a school bus?","What is the longest car trip on one tank of gas?"];
-    this.correctAnswers = ["KLM","Port of Shanghai","337.60 km","590 km/h","2617 km"];
-    this.answers = [];
-    this.options = [["Avianca","KLM","Qantas"],
-              ["Port of Shanghai","Port of Singapore","Port of Rotterdam"],
-              ["89.30 km","675.10 km","337.60 km"],
-              ["590 km/h","320 km/h","245 km/h"],
-              ["2617 km","3568 km","1732 km"]];
-    this.callback = null;*/
+
